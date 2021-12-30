30db92e4865d98735f6b411af2c7d2616ac894be
ejackson007 <evanojackson@gmail.com>
2021-12-30 16:13:13 -0600

    snake

--- main.py
+++ main.py
@@ -1,40 +1,6 @@
-# Import the pygame module
 import pygame
 
-# Import pygame.locals for easier access to key coordinates
-# Updated to conform to flake8 and black standards
-from pygame.locals import (
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
-    K_ESCAPE,
-    KEYDOWN,
-    QUIT,
-)
+if __name__ == "__main__":
+    pygame.init()
 
-# Initialize pygame
-pygame.init()
-
-# Define constants for the screen width and height
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
-# Create the screen object
-# The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-# Function to keep the game running while using it
-running = True
-
-#Main loop
-while running:
-    # Look at every event in the queue
-    for event in pygame.event.get():
-        # Did the user hit a button? 
-        if event.type == KEYDOWN:
-            # Was it the Escape key? If so, stop the loop.
-            if event.key == K_ESCAPE:  
-                running = False
-        elif event.type == QUIT:
-            running = False
-
+    surface = pygame.display.set_mode()
