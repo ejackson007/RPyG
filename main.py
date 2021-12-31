6065473de5ccc68f924b450408a51c32b5092dcf
ejackson007 <evanojackson@gmail.com>
2021-12-31 00:30:59 -0600

    snake moves and can grow

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
 import pygame
 from pygame.locals import *
 
-def draw_block():
-    surface.fill((110,110,5))
-    surface.blit(block,(block_x, block_y))
-    #update screen
-    pygame.display.flip()
+def move_snake(x, y):
+    surface.blit(bg, (0,0))
+    '''snake works mostly in the way of follow the leader. This can be implemented by
+    simply adding on one in the front and deleting the one in the back since each piece
+    of the same size'''
+    print(snake[-1])
+    snake.append([snake[-1][0] + x, snake[-1][1] + y])
+    print(snake)
+    del(snake[0])
+    print(snake)
+    for rib in snake:
+        surface.blit(block, (rib[0], rib[1]))
+
+def add_rib():
+    '''adds a rib to the snake. This is done by appending it to the front of the list, which 
+    is the back of the snake. The side is determined by the relationship of the first 2 locations in the list.
+    This is done so that the movement of the snake still feels natural. '''
+    move_x = snake[0][0] - snake[1][0]
+    move_y = snake[0][1] - snake[1][1]
+    print(f"adding {move_x} {move_y}")
+    nrib = [snake[0][0] + move_x, snake[0][1] + move_y]
+    snake.insert(0, nrib)
+    print(snake)
+    for rib in snake:
+        surface.blit(block, (rib[0], rib[1]))
+
 
 if __name__ == "__main__":
     pygame.init()
 
-    surface = pygame.display.set_mode((500,500))
+    surface = pygame.display.set_mode((400,400))
     #set background to white
     surface.fill((110,110,5))
 
+    bg = pygame.image.load("resources/background.jpg").convert()
+    surface.blit(bg, (0,0))
+
     block = pygame.image.load("resources/block.jpg").convert()
-    block_x = 100
-    block_y = 100
-    surface.blit(block,(block_x, block_y))
+    #list that stores where the snake is
+    snake = [[0,0], [0,40]]
     #update screen
     pygame.display.flip()
     #event loop
@@ -28,16 +51,15 @@ if __name__ == "__main__":
                 if event.key == K_ESCAPE:
                     running == False
                 elif event.key == K_RIGHT:
-                    block_x = block_x+20
-                    draw_block()
+                    move_snake(40,0)
                 elif event.key == K_LEFT:
-                    block_x = block_x-20
-                    draw_block()
+                    move_snake(-40,0)
                 elif event.key == K_UP:
-                    block_y = block_y-20
-                    draw_block()
+                    move_snake(0, -40)
                 elif event.key == K_DOWN:
-                    block_y = block_y+20
-                    draw_block()
+                    move_snake(0, 40)
+                elif event.key == K_SPACE:
+                    add_rib()
             elif event.type == QUIT:
                 running = False
+            pygame.display.flip()
