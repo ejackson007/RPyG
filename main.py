6af7e3a0081642e3fc8299c6ed2c950b62f4e2c4
ejackson007 <evanojackson@gmail.com>
2022-01-03 12:18:22 -0600

    score overwriting

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
 from pygame.locals import *
+import pygame.freetype
 import time
 from random import randint
+import os
 
 def move_snake():
     surface.blit(bg, (0,40))
@@ -9,24 +11,25 @@ def move_snake():
     '''snake works mostly in the way of follow the leader. This can be implemented by
     simply adding on one in the front and deleting the one in the back since each piece
     of the same size'''
-    print(snake[-1])
+    #print(snake[-1])
     snake.append([snake[-1][0] + x, snake[-1][1] + y])
-    print(snake)
+    #print(snake)
     del(snake[0])
-    print(snake)
+    #print(snake)
     for rib in snake:
         surface.blit(block, (rib[0], rib[1]))
 
-def add_rib():
+def add_rib(score):
     '''adds a rib to the snake. This is done by appending it to the front of the list, which 
     is the back of the snake. The side is determined by the relationship of the first 2 locations in the list.
     This is done so that the movement of the snake still feels natural. '''
+
     move_x = snake[0][0] - snake[1][0]
     move_y = snake[0][1] - snake[1][1]
-    print(f"adding {move_x} {move_y}")
+    #print(f"adding {move_x} {move_y}")
     nrib = [snake[0][0] + move_x, snake[0][1] + move_y]
     snake.insert(0, nrib)
-    print(snake)
+    #print(snake)
     for rib in snake:
         surface.blit(block, (rib[0], rib[1]))
 
@@ -44,12 +47,12 @@ def fruitloc():
 
 def out_of_bounds():
     if 0 > snake[-1][0] or snake[-1][0] >= 400:
-        print(True)
+        #print(True)
         return True
     elif 40 > snake[-1][1] or snake[-1][1] >= 440:
-        print(True)
+        #print(True)
         return True
-    print(False)
+    #print(False)
     return False
 
 if __name__ == "__main__":
@@ -58,6 +61,14 @@ if __name__ == "__main__":
     surface = pygame.display.set_mode((400,440))
     #set background to white
     surface.fill((110,110,5))
+
+    #get font and write Score at top
+    score = 0
+    fontpath = "/System/Library/Fonts/Geneva.ttf"
+    pygame.freetype.init()
+    fsize = 50
+    myfont = pygame.freetype.Font(fontpath, fsize)
+    myfont.render_to(surface, (0,0), f"Score: {score}", (0,0,0), None, size=50)
 
     bg = pygame.image.load("resources/background.jpg").convert()
     surface.blit(bg, (0,40))
@@ -97,14 +108,18 @@ if __name__ == "__main__":
                     x = 0
                     y = 40
                 elif event.key == K_SPACE:
-                    add_rib()
+                    score += 1
+                    myfont.render_to(surface, (0,0), f"Score: {score}", (0,0,0), None, size=50)
+                    add_rib(score)
             elif event.type == QUIT:
                 running = False
         time.sleep(.15)
         move_snake()
         #makes contact with fruit
         if [fx, fy] in snake:
-            add_rib()
+            score += 1
+            myfont.render_to(surface, (0,0), f"Score: {score}", (0,0,0), None, size=50)
+            add_rib(score)
             fx, fy = fruitloc()
 
         #losing situation
